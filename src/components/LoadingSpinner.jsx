0e7146760d6b958072f42a90fefa46448d3dc96a
Tanmay Shende <tanmayshende007@gmail.com>
2025-09-23 22:50:43 +0530

    feat: loading component added

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,8 @@
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center p-8">
+    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+    <span className="ml-3 text-gray-600">Processing data...</span>
+  </div>
+);
+
+export default LoadingSpinner;
